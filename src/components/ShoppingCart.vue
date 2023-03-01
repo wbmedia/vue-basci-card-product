dfc8d892492d79a9150aa771d0d8ef7c75495eaa
Antonio Nicasio <antonio.nicasio.herrera@gmail.com>
2023-03-01 15:13:58 -0800

    end basic prodcut card cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -43,6 +43,10 @@ function addToCart() {
   cart.value++
 }
 
+function clearCart() {
+  cart.value = 0
+}
+
 function updateImage(variantImage) {
   tenisImage.value = variantImage
 }
@@ -180,8 +184,15 @@ onMounted(() => {
             d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
           />
         </svg>
-        Add to cart &nbsp; {{ cart }}</a
+        Add to cart &nbsp; ({{ cart }})</a
       >
+      <a
+        href="#"
+        @click="clearCart"
+        class="flex mt-1 items-center justify-center rounded-md bg-slate-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+      >
+        Clear Cart
+      </a>
     </div>
   </div>
 </template>
